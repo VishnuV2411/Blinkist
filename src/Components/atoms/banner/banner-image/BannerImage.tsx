4e9bbcacc5d99ef92dbfa8ea2065654c544d0af6
VishnuV2411 <vishnuvardhan.t@zemosolabs.com>
2022-03-16 09:12:33 +0530

    resolved code smell

--- src/Components/atoms/banner/banner-image/BannerImage.tsx
+++ src/Components/atoms/banner/banner-image/BannerImage.tsx
@@ -1,10 +1,9 @@
 import { Card, CardMedia } from "@mui/material";
 import React from "react";
 import bannerimg from "../../assets/bannerimg.png";
-import customStyle from "../../../../Theme";
+
 
 export const BannerImage = () => {
-  const classes = customStyle();
   return (
     <>
       <Card>
